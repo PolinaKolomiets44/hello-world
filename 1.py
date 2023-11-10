93431d346672a56058f7afbdc7105e441f69e312
PolinaKolomiets44 <150428818+PolinaKolomiets44@users.noreply.github.com>
2023-11-10 16:58:33 +0700

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,17 @@
+﻿
+for column in range(1,11):
+    if column < 10:
+        new_number = []
+        new_number.append(f"умножение на {column}||")
+        for  line in range(1,11):
+            if line < 10:
+                num = column * line
+                new_number.append(num)
+               
+            else:
+                print (new_number)
+                continue
+    else:
+        break
+
+    
